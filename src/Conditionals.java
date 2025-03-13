19ac6d49a5ba6a8b7f48b2ebef457f3193a79712
Rinomaione <maionegaspare95@gmail.com>
2025-03-13 20:39:23 +0100

    commit iniziale

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -24,17 +24,35 @@ public class Conditionals {
 		//Se hai meno di 15 anni, il costo sarà uguale a 50
 		//Se hai tra i 15 e i 20 anni, il costo sarà 70
 		//Se hai tra i 20 e i 32 anni, il costo sarà 200
-		
+		// se hai piu di 32 , il costo sara 300
+
+
+		int time =18: //ora spettacolo del biglietto 
+		//il prezzo del biglietto , se l orario e  dopo le 18, 10% di sconto
+
+		//da 0 a 15
 		if(age < 15){
 		  cost = 50;
 		}
-		else if(age > 15 && age < 20){
+		//tra 16 e 20
+		else if(age < 20){
 		  cost = 70;
 		}
-		else if(age > 20 && age < 32){
+		//tra 21 e 32
+		else if(age < 32){
 		  cost = 200;
 		}
-		
+		//da 33 in poi
+		else{
+		  cost =300;
+
+		if (time>18){
+		cost*= cost 0.9:
+		}
+
+		//int a = 0;
+		//if()
+
 		System.out.println(cost);
 	}
 }
