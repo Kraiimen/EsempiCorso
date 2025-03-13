656acb11887511771a445c9dc2691e701b75c620
N9C9 <nicolo.casertano@gmail.com>
2025-03-13 17:30:33 +0100

    commit test 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		System.out.println("aggiunto per testare git e github");
-		System.out.println("aggiunto da Nicolò per testare la branch!!");
+		System.out.println("aggiunto da Nicolò per testare la branch!");
 	}	
 }
