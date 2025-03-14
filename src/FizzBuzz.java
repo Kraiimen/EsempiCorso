f1dbec403899e1ca119e080f1164c353b01d3621
EmanueleGiustiniani <giustinianiemanuele0@gmail.com>
2025-03-14 21:25:47 +0100

    stampa di FizzBuzz per i multipli di 3,5

---
+++ src/FizzBuzz.java
@@ -0,0 +1,22 @@
+// stampa i primi 100 numeri
+public class FizzBuzz{
+    public static void main(String[] args) {
+        for(int i=0; i<101; i++){
+            if(i%3 == 0 && i%5 == 0){                       // se il numero è un multiplo sia di 3 che di 5 stampa fizzbuzz
+                System.out.println("fizzbuzz");
+            }
+            else if(i%3 == 0){                              // se il numero è un multiplo di 3 stampa fizz
+                System.out.println("fizz");
+            }
+            else if(i%5 == 0){                              // se il numero è un multiplo di 5 stampa buzz
+                System.out.println("buzz");
+            }
+            if(i%3 !=0 && i%5 != 0){                        // se il numero non è un multiplo di 5 o 3, allora stampa il numero
+                System.out.println(i);
+            }
+        }
+    }
+
+}
+    
+
