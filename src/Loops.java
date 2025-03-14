ce979323e164443b95eb41ed777a27b086614330
Kraiimen <78323935+Kraiimen@users.noreply.github.com>
2025-03-14 15:06:46 +0100

    cicli for

---
+++ src/Loops.java
@@ -0,0 +1,24 @@
+public class Loops {
+    public static void main(String[] args){
+
+        //Ciclo for che stampa i numeri dispari da 1 a 100
+
+        // for(int i=1; i<101; i++){
+        //     if(i % 2 != 0){
+        //         System.out.println(i);
+        //     }
+        // }
+
+        
+        // for(int i=1; i<101; i+=2){
+        //     System.out.println(i);
+        // }
+
+
+        //Multipli di 5 da 100 a 0
+        for(int i=100; i>0; i-=5){
+            System.out.println(i);
+        }
+
+    }
+}
