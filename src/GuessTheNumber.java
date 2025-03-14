57cbac3a30f13daff2c9d0c81db92ff933dd49f1
Zomerzz <aresufilippo2005@gmail.com>
2025-03-14 20:21:53 +0100

    commit automatico

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -6,25 +6,23 @@ public class GuessTheNumber {
         Questo programma chiede al utente di indovinare il numero misterioso
         se l'utente azzecca dirà "bravo lo hai indovinato", se l'utente dice un numero più basso, il programma risponde dicendo "sbagliato, il numero è più grande" e vice versa con un numero più grande
         il programma deve anche tenere traccia del numero di tentativi
-        
+
         */
-        Random dice = new Random();
-        int n = dice.nextInt(10001);
-        Console console = System.console();
+        Random dice = new Random(); //crea un oggetto di tipo Random, che useremo per generare un numero casuale
+        int n = dice.nextInt(10001); // inseriamo il numero Random in n
+        Console console = System.console(); //Crea un oggetto di tipo Console, servirà in successivo per richiedere imput
         System.out.println("indovina il numero tra 0 e 10000");
         int N;
         int tentativi=0;
         do{
-            String NI = console.readLine();
-            N = Integer.parseInt(NI);
-            if(N> 10000 || N<1){
-                System.err.println("ricorda che è tra 0 e 10000");
+            String NI = console.readLine(); //crea la variabile NG (Numero inserito)
+            N = Integer.parseInt(NI); //Passiamo il valore di Numero Inserito in N (Numero)
+            if(N> 10000 || N<=0){ // Check che il numero sia in range
+                System.out.println("ricorda che il numero è tra 0 e 10000");
             }else if(N == n && tentativi ==1){
-                System.out.println("WOW al primo tentativo?");
-                tentativi =0;
+                System.out.println("WOW al primo tentativo? impressionante");
             }else if(N == n && tentativi !=1){
                 System.out.println("Hey hai indovinato! in " + tentativi + " tentativi!");
-                tentativi =0;
             }else if (N > n){
                 System.out.println("no, il numero è più piccolo");
             }else{
@@ -32,6 +30,6 @@ public class GuessTheNumber {
             }
             tentativi++;
         }while(N != n);
-        
+        tentativi =0;
     }
 }
