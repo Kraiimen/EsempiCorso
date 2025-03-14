47fcddd10fafa963846d1452ec93194686281c68
Zomerzz <aresufilippo2005@gmail.com>
2025-03-14 15:41:55 +0100

    Loops

--- src/Loops.java
+++ src/Loops.java
@@ -1,8 +1,22 @@
+import java.io.Console;
 public class Loops {
     public static void main (String[] args){
-        
-        for(int i = 0; i<10; i++){
+        Console console = System.console();
+        // for(int i = 0; i<10; i++){
+        //     System.out.println(i);
+        // }
+        // fare i un ciclo che stampa i numeri dispari tra 1 e 100
+        for(int i=1; i<=100; i+=2){
             System.out.println(i);
         }
+        String answer;
+        do{
+            System.out.println("Inserisci una risposta di tipo Yes or No");
+            answer = console.readLine();
+            if(!answer.equals("Yes") && !answer.equals("No")){
+                System.out.println("Ti ho detto di risposndere Yes or No!");
+            }
+        }while (!answer.equals("Yes") || !answer.equals("No"));
+        
     }
 }
