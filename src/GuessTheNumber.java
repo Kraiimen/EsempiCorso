c9a8318b47a9be3149a822a781d8bf46e76be0e0
EmanueleGiustiniani <giustinianiemanuele0@gmail.com>
2025-03-14 17:05:54 +0100

    Genera un numero random e lo confronta con un input da tastiera

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -1,26 +1,32 @@
+//importa le librerie
 import java.io.Console;
 import java.util.Random;
 
+//utente da un input, se sbaglia ad indovinare il numero random, indicare se minore o maggiore di questo
+//segnare il numero di tentativi e stamparlo
+
 public class GuessTheNumber{
         public static void main(String[] args){
-        Random dice = new Random();
-        int n=dice.nextInt(100);
-        int input=0;
-        int tentativi=0;
-        //utente da un input, se sbaglia ad indovinare il numero random, indicare se minore o maggiore di questo
-        //segnare il numero di tentativi e stamparlo
-        Console console = System.console();
+        Random dice = new Random();                 //crea un oggetto di nome "dice" e di tipo Random
+        int n=dice.nextInt(100);                    //crea una variabile di tipo int e di nome "n" e inserisce al suo interno un numero intero randomico tra 0 e 100
+        int input=0;                                //inizializza una variabile di nome "input" che servirà per contenere l'intero inserito da tastiera
+        int tentativi=0;                            //inizializza una variabile di nome "tentativi" che verrà utilizzata come contatore
+        Console console = System.console();         //crea un oggetto di nome "console" e di classe Console
+
+        //inizio ciclo 
         do{
-            System.out.println("Inserisci un numero:  ");
-            String ns = console.readLine();
-            input = Integer.parseInt(ns);
-            tentativi +=1;
+            System.out.println("Inserisci un numero:  ");                                                   //chiede d'inserire un input da tastiera
+            String ns = console.readLine();                                                                 //crea una variabile di tipo String e di nome "ns" dentro la quale viene inserita la stringa che l'utente scrive da tastiera
+            input = Integer.parseInt(ns);                                                                   //all'interno della variabile input, viene inserito il contenuto di ns convertito in Int
+            tentativi +=1;                                                                                  //il numero di tentativi viene aumentato di 1
+
+            //controllo se l'input da tastiera(input) è maggiore o minore del numero generato(n)
             if(input < n){
                 System.out.println("hai sbagliato! Il numero magico è maggiore di quello inserito!");
             }else if(input > n){
                 System.out.println("hai sbagliato! Il numero magico è minore di quello inserito!");
             }
-        }while(n != input);
-        System.out.println("hai impiegato " + tentativi + " tentativi.");
+        }while(n != input);                                                                                  //il ciclo finisce quando il numero random coincide con l'input dato dalla tastiera
+        System.out.println("hai impiegato " + tentativi + " tentativi.");                                    //stampa il numero di tentativi
     }
 }
