2054eabd71ca06ab590f699dc43bdf63ca07aa1d
OniTusk <lazyorpheus@gmail.com>
2025-03-14 16:15:20 +0100

    idk man

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class GuessTheNumber {
-    public static void main (Sting [] args) {
+    public static void main (String [] args) {
 
             Random dice = new Random();
             int n = dice.nextInt (10_001);
