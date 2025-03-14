950ba51c8424eb272f9eb18f815d0791e8bc5523
Vittorio787 <vittorioaquila.nervi@gmail.com>
2025-03-14 18:14:49 +0100

    src/GuessTheNumber.java

---
+++ src/GuessTheNumber.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+
+        Random dice = new Random();
+        int n = dice.nextInt(100_001);
+        //continuare questo programma. Deve chiedere un numero misterioso, avere la risposta
+        //se invece il numero dell'utente è sbagliato ed è minore, stampa "Hai sbagliato, il numero è più piccolo"
+        //Caso inverso della seconda riga
+        //Il programma tiene conto del numero dei tentativi e li riporti a fine del processo il numero di tentativi.
+    }
+}
