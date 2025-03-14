c1ca19690db8c11319bce12e3242fa4dfb59e340
EManganelli <manganelli.eugenio@outlook.com>
2025-03-14 16:25:02 +0100

    commit proprio perchè devo

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -1,6 +1,7 @@
 public class Conditionals {
 	public static void main(String[] args){
 		
+		/*
 		int x = 10;
 
 		if(x == 3){
@@ -16,25 +17,24 @@ public class Conditionals {
 		  System.out.println("X è diverso da 3, 5 e 7");
 		  System.out.println("Che sfiga!");
 		}
+		*/
+		int age = 14;
+        double cost = 0;
 
-		
-		int age = 32;
-		double cost = 0;
-
-		//Se hai meno di 15 anni, il costo sarà uguale a 50
-		//Se hai tra i 15 e i 20 anni, il costo sarà 70
-		//Se hai tra i 20 e i 32 anni, il costo sarà 200
-		
-		if(age < 15){
-		  cost = 50;
-		}
-		else if(age > 15 && age < 20){
-		  cost = 70;
-		}
-		else if(age > 20 && age < 32){
-		  cost = 200;
-		}
-		
-		System.out.println(cost);
-	}
+        //Se hai meno di 15 anni, il costo sarà uguale a 50
+        //Se hai tra i 15 e i 20 anni, il costo sarà 70
+        //Se hai tra i 20 e i 32 anni, il costo sarà 100 
+        if (age <= 15) {
+            cost = 50;
+        } else if (age <= 20) {
+            cost = 70;
+        } else if (age <= 32) {
+            cost = 100;
+        } else {
+            cost = 200;
+        }
+        System.out.println(cost);
+    }
 }
+		
+	 
