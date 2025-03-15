ea14e961c116ab1f445f4024b68fb327c2d5f7b5
EmanueleGiustiniani <giustinianiemanuele0@gmail.com>
2025-03-15 11:11:19 +0100

    Input massimo ed exit con q

--- src/StopInput.java
+++ src/StopInput.java
@@ -5,7 +5,10 @@ public class StopInput {
     public static void main(String[] args) {
         //inizializza le variabili "input"(contenitore della stringa inserita da tastiera) e "max"(intero dentro il quale andrà il numero massimo inserito)
         String input;                                                 
-        int max = 0;
+        int max=0;
+
+
+        boolean PrimoInput = true;                                                         //crea una variabile di tipo boolean e la imposta a true
 
         Console console = System.console();                                                //crea un oggetto di nome "console" e di classe Console
 
@@ -20,12 +23,24 @@ public class StopInput {
             if(!input.equals("q")){                        //condizione necessaria a capire se la stringa inserita contiene un qualcosa di diverso da "q"
                 int num = Integer.parseInt(input);         //inizializzazione di "num" all'interno del quale viene inserita la stringa numerica convertita in intero
 
+                //Il primo numero inserito all'interno del programma diventa il numero massimo
+                if(PrimoInput){                            
+                    max = num;
+                    PrimoInput = false;                    //PrimoInput diventa false perchè è stato inserito dentro max il primo numero ricevuto da tastiera
+                }
+
                 //se il numero inserito è maggiore di quello inserito in precedenza, allora diventa lui quello più grande
                 if(num > max){
                     max = num;
                 }
             }
         }while(!input.equals("q"));            //il tutto continua fino a quando non viene inserito "q" da tastiera
-        System.out.println(max);
+
+        //se non è stato inserito nessun numero e max è quindi uguale a 0, allora stampa "Non hai inserito nessun numero", altrimenti stampa il numero massimo inserito
+        if(max == 0){       
+            System.out.println("Non hai inserito nessun numero");
+        }else{
+            System.out.println("numero massimo inserito: " + max);
+        }
     }
 }
