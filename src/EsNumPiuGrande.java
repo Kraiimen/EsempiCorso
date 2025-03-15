c127ffd4bfe70df204a6d49af23c61a88263af57
Colum-nar <edoardobognanni@gmail.com>
2025-03-15 19:10:23 +0100

    prova

---
+++ src/EsNumPiuGrande.java
@@ -0,0 +1,35 @@
+// Traccia esercizio 3:
+// Un programma che all'avvio chieda un numero, 
+// poi chiede un altro numero o Q per smettere di inserire 
+// (e cosi via finché non viene inserito Q)
+// Alla fine il programma stampa il numero più grande ricevuto
+import java.io.Console;
+
+
+public class EsNumPiuGrande{
+
+    public static void main(String[] args){
+        String exitChar = "q"; //carattere di uscita
+        String userAnswer = null; 
+        Console console = System.console(); //oggetto che utilizzeremo per prendere gli input dall utente
+        System.out.print("Scrivi un numero: ");
+        String primoInput = console.readLine();
+        int nPiuGrande = Integer.parseInt(primoInput);
+        
+
+        do{
+            System.out.print("Inserisci un altro numero o \"q\" per uscire: ");
+            userAnswer = console.readLine();
+            if (userAnswer.equals(exitChar)){ 
+                //System.out.println("OI");
+                break; //esce dal do-while quando eseguita
+            }
+            int n2 = Integer.parseInt(userAnswer);
+            if(nPiuGrande < n2){
+                nPiuGrande = n2;
+            }
+        }while(!userAnswer.equals(exitChar));
+        System.out.println("Il numero piu grande che hai messo è: "+ nPiuGrande);
+        
+    }
+}
--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -8,52 +8,53 @@ public class GuessTheNumber {
         Random dice = new Random();
         int n = dice.nextInt(100_001);
         int ourChoice;
+        int tentativo = 0;
         //continuare questo programma. Deve chiedere un numero misterioso, avere la risposta e: se azzecca stampa "bravo sei un genio"
         // se invece il numero dell'utente è sbagliato ed è minore, stampa "Hai sbagliato, il numero magico è maggiore"
         // se il numero è sbagliato ed è più piccolo stampa "Hai sbagliato, il numero è più piccolo"
         // voglio che il programma conti i tentativi e li riporti a fine del processo il numero dei tentativi
-        // do{
-        //     System.out.println("Vediamo se riesci a indovinare il mio stupido numero");
-        //     String ns = console.readLine()
-        //     System.out.println("Il mio numero segreto è " + n);
-        //     ourChoice = Integer.parseInt(ns);
-        //     if (n > ourChoice){
-        //         System.out.println("Il numero è troppo piccolo");
-        //     } else if (n < ourChoice){
-        //         System.out.println("Il numero è troppo grande");
-        //     }
-        //     
-        // } while(ourChoice != n);
-        // System.out.println("Era ora umano");
+        do{
+            System.out.println("Vediamo se riesci a indovinare il mio stupido numero");
+            String ns = console.readLine();
+            tentativo++;
+            //System.out.println("Il mio numero segreto è " + n);
+            ourChoice = Integer.parseInt(ns);
+            if (n > ourChoice){
+                System.out.println("Il numero che mi hai dato è troppo piccolo");
+            }else if (n < ourChoice){
+                System.out.println("Il numero che mi hai dato è troppo grande");
+            }
+        } while(ourChoice != n);
+        System.out.println("Era ora umano, hai fatto "+ tentativo + " tentativi");
         
         for(int i = 1; i < 100; i++){
             //metodo esotico
-            if (i % 3 == 0){
-                System.out.print("fizz");
-            }
-            if (i%5 == 0){
-                System.out.print("buzz");
-            }
-            if (!(i%5 == 0) && !(i%3 == 0)){
-                System.out.print(i);
-                
-            }
-            System.out.println("");
-            //Metodo giusto
-            if ((i%5 == 0) && (i%3 == 0)){
-                System.out.println("fizzbuzz");
-                
-            }else if(i%3 == 0){
-                System.out.println("fizz");
-            }else if (i%5 == 0){
-                System.out.println("buzz");
-            else{
-                System.out.println(i);
-            }
+                // if (i % 3 == 0){
+                //     System.out.print("fizz");
+                // }
+                // if (i%5 == 0){
+                //     System.out.print("buzz");
+                // }
+                // if (!(i%5 == 0) && !(i%3 == 0)){
+                //     System.out.print(i);
+                    
+                // }
+                // System.out.println("");
+                //Metodo giusto
+                if ((i%5 == 0) && (i%3 == 0)){
+                    System.out.println("fizzbuzz");
+                    
+                }else if(i%3 == 0){
+                    System.out.println("fizz");
+                }else if (i%5 == 0){
+                    System.out.println("buzz");
+                }else {
+                    System.out.println(i);
+                }
             }
         }
 
 
     }
     //programma che stampa i primi 100 numeri se è un multiplo di 3 stampa fix se è un multiplo di 5 buzz se è un multiplo sia di 3 che di 5 fix buzz
-}
+
