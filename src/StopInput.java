535433f7bdc6080af2a219031cc02fcc82d9f624
EmanueleGiustiniani <giustinianiemanuele0@gmail.com>
2025-03-15 10:03:22 +0100

    Input massimo ed exit con q

---
+++ src/StopInput.java
@@ -0,0 +1,35 @@
+// chiede un numero
+// dammi un altro numero oppure premi q per smettere
+// alla fine ti stampa il numero piu grande che hai scritto
+
+import java.io.Console;
+
+public class StopInput {
+    public static void main(String[] args) {
+        //inizializza le variabili "input"(contenitore della stringa inserita da tastiera) e "max"(intero dentro il quale andrà il numero massimo inserito)
+        String input;                                                 
+        int max = 0;
+
+        Console console = System.console();                                                //crea un oggetto di nome "console" e di classe Console
+
+        //inizio ciclo do while
+        do{
+            //richiesta numero o lettera q, da inserire da tastiera
+            System.out.println("premere   q   se si desidera terminare il programma!");
+            System.out.println("Inserire un numero: ");
+
+            
+            input = console.readLine();                    //utilizzo del metodo readLine() per leggere la stringa inserita ed inserirla nella variabile input
+            if(!input.equals("q")){                        //condizione necessaria a capire se la stringa inserita contiene un qualcosa di diverso da "q"
+                int num = Integer.parseInt(input);         //inizializzazione di "num" all'interno del quale viene inserita la stringa numerica convertita in intero
+
+                //se il numero inserito è maggiore di quello inserito in precedenza, allora diventa lui quello più grande
+                if(num > max){
+                    max = num;
+                    System.out.println(max);
+                }
+            }
+        }while(!input.equals("q"));            //il tutto continua fino a quando non viene inserito "q" da tastiera
+        System.out.println(max);
+    }
+}
