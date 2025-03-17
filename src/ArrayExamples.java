94f7682ba6310251523ac939912ee0ab4672e71b
N9C9 <nicolo.casertano@gmail.com>
2025-03-17 14:10:02 +0100

    array1

--- src/ArrayExamples.java
+++ src/ArrayExamples.java
@@ -1,15 +1,14 @@
 
-import javax.print.attribute.standard.NumberUpSupported;
+public class ArrayExamples{
+    public static void main(String[] args){
+        int[] numbers = new int[5];
+        // int numbers2[] = new int[5];
 
-public class ArrayExamples {
-    public static void main(String[] args) {
-        
-        int[] numbers = new int [5];
-        // int numbers2 [] = new int[5];
         // int x = 10;
         // int[] numbers3 = new int[x];
         // System.out.println(numbers[0]);
         // System.out.println(numbers[4]);
+
         // System.out.println(numbers[5]);
 
         // ci possiamo spostare da una posizione all'altra istantaneamente
@@ -27,7 +26,7 @@ public class ArrayExamples {
         // voglio inserire nell'array numbers, i quarati dei primi 5 numeri
 
 
-        for(int i = 0; i < NumberUpSupported.length; i++) {
+        for(int i = 0; i < numbers.length; i++) {
             numbers[i] = (i + 1)*(i + 1);
         }
         for(int n : numbers) {
@@ -42,3 +41,5 @@ public class ArrayExamples {
 
     }        
 }
+
+
