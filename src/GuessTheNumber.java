0591b61fe69b90e35aabbb6f8cc291622019c54a
N9C9 <nicolo.casertano@gmail.com>
2025-03-17 10:12:19 +0100

    corregge un bug in GuessTheNumber

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -6,39 +6,42 @@ public class GuessTheNumber {
 
         Console console = System.console();
         Random dice = new Random();
-        int n = dice.nextInt(100_001);
+        int n = dice.nextInt(100); // real magic number
         // continuare questo programma. Deve chiedere un numero misterioso, se azzecca stampa "bravo sei un genio"
         // se invece il numero dell'utente è sbagliato ed è minore, stampa "Hai sbagliato, il numero magico è maggiore"
         // se il numero è sbagliato ed è più piccolo stampa "Hai sbagliato, il numero è più piccolo"
         // voglio che il programma conti i tentativi e li riporti a fine del processo il numero dei tentativi
 
         String ns;
-        int y = 1; 
-        int mN = 53790; 
+        int y = 1; //mettere nome più preciso sullo scopo di quella variabile
+        // int mN = 53790; 
+        int iP; //dichiarazione di iP senza valore per ora
         do {
             
             System.out.println("Dammi un numero qualsiasi da 1 a 100.000");
 
             ns = console.readLine(); 
-            int iP = Integer.parseInt(ns);
+            iP = Integer.parseInt(ns);
             
 
-            if(iP < mN) {
+            if(iP < n) {
                 System.out.println("Hai sbagliato, il numero magico è più grande");  
-                y++;
+                
 
             } 
-            else if (iP > mN) {
+            else if (iP > n) {
                 System.out.println("Hai sbagliato, il numero magico è più piccolo");   
-                y++;              
+                          
 
             }
-            else if (iP == mN) {
+            else if (iP == n) {
                 System.out.println("Congratulazioni! Hai azzeccato il numero magico!");                   
                 System.out.println("Tentativi: " + y);
             }
-            
-        } while (!ns.equals("53_790"));  
+
+            y++; 
+
+        } while (iP != n); 
         
     }
 
