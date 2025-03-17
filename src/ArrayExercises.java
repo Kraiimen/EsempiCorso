9b420ddc9f1ecf7c985b3b818ed9e70b88ce3152
OniTusk <lazyorpheus@gmail.com>
2025-03-17 17:26:38 +0100

    Esempi Array e creazione degli esercizi degli array

---
+++ src/ArrayExercises.java
@@ -0,0 +1,16 @@
+public class ArrayExercises {
+    public static void main(String[] args){
+// es 1: creare un array di 5 interi e inizializzarlo con i primi numeri da 1 a 5 al quadrato
+// es 2 calcolare e stampare il valore della somma di tutti i numeri dell'array 
+// es 3 calcolare e stampare il valore minimo contenuto nell'array
+// es 4 calcolare il valore medio contenuti  nell'array (somma dei valori, sarà un double)
+// es 5 calcolare e stampare il numero di multipli di 2 che esistono all'interno dall'array (multipli di due)
+// es 6 dato un valore arbitrario(int x = 4) calcolare e stampare il numero di volte in cui l'array contiene il valore di x
+// es 7 creare un codice che inverta l'array
+
+// es 8 dato un numero arbitrario (int z = 8) verificare quante volte nell'array succede che ne''array ci sono due numeri consecutivi in cui la somma fa z
+// es 9 verificare che nell'array non esistano 3 numeri consecutivi la cui somma è maggiore di 12
+// es 10 trovare il numero in cui appare più volte nell'array (2, 7, 12, 2, 7) va bene anche uno dei due
+
+    }
+}
