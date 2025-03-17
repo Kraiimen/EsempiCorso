aa5b213efad66e666af2c5c1a547a104b1114b95
pixscar <pie.dx96@gmail.com>
2025-03-17 09:52:05 +0100

    GuessTheNumber

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -1,3 +1,4 @@
+import java.io.Console;
 import java.util.Random;
 
 public class GuessTheNumber {
@@ -5,12 +6,38 @@ public class GuessTheNumber {
         
         Random dice = new Random();
         int n = dice.nextInt(100_001);
+		Console console = System.console();
+		if (console == null) {
+			System.out.println("Console non disponibile, utilizza un terminale");
+			return ;
+		}
+		int tries = 0;
+		int num = -1;
+		System.out.println("Dammi un numero qualsiasi tra 0 e 100000");
+		//System.out.print("Aiutino per test, il numero è");
+		//System.out.println(n);
+		while (num != n) {
+			System.out.println("Indovina il numero:");
+			String tentativo = console.readLine();
+        	num = Integer.parseInt(tentativo); //passami in intero la stringa del tentativo
+			if (num > n) {
+				System.out.println("Sbagliato, il numero magico è più piccolo!");
+			}
+			else if (num < n) {
+				System.out.println("Sbagliato, il numero magico è più grande");
+			}
+			else {
+				System.out.println("Bravo, hai indovinato, sei un genio!");
+			}
+			tries++;
+		}
+		System.out.print("Il numero di tentativi è stato:");
+		System.out.println(tries);
+    }
+    
+}
+
         //continuare questo programma. Deve chiedere un numero misterioso, avere la risposta e: se azzecca stampa "bravo sei un genio"
         // se invece il numero dell'utente è sbagliato ed è minore, stampa "Hai sbagliato, il numero magico è maggiore"
         // se il numero è sbagliato ed è più piccolo stampa "Hai sbagliato, il numero è più piccolo"
         // voglio che il programma conti i tentativi e li riporti a fine del processo il numero dei tentativi
-
-        
-    }
-    
-}
