aca32a8dea93101243e1a8217ac24f31da3001b1
pixscar <pie.dx96@gmail.com>
2025-03-19 11:38:00 +0100

    Methods piero_scarcina

--- src/Methods.java
+++ src/Methods.java
@@ -21,6 +21,22 @@ public class Methods{
 		System.out.println(result); //3 volte la stessa cosa, semplificando sempre
 
 		//Questo codice (quello commentato ora) funziona ma è molto ripetitivo
+
+
+		greet();
+		int nameLen = greetWithName("Piero");  //salvo la lunghezza del nome in una variabile int
+		System.out.println(nameLen);
+		nameLen = greetWithName("Sabrina");
+		System.out.println(nameLen);
+
+		boolean even = isEven(13);
+		System.out.println(even);
+
+		printLenAndUpperCaseLowerCase("Bella zio bellaaaaa!!");
+
+		String longer = getLonger("Bella zio", "Bella zione");
+		System.out.println(longer);
+		
 	}
 
 	public static int readInteger() {
@@ -41,6 +57,51 @@ public class Methods{
 	//Voglio una funzione che stampi La somma dei due numeri è
 	public static void printMessage() {
 		System.out.print("La somma dei due numeri è ");
+	}
+
+
+	// quello che segue è la definizione della funzione greet
+	public static void greet() {
+		// la keyword public permette alla funzione di essere chiamata anche al di fuori di questa classe
+		System.out.println("è un piacere conoscerti!"); 
+	}
+
+	
+	public static int greetWithName(String name) {
+		System.out.println("è un piacere conoscerti " + name);
+		int len = name.length();  // la lunghezza del nome
+		return len;  //ritorna a chi la chiama la lunghezza del nome
+	}
+
+	public static boolean isEven(int x) {
+ /*
+		if (x % 2 == 0) {
+			return true;
+		} else {
+			return false; // il return deve esserci in entrambi i casi, perché la funzione deve tornare sempre una booleana
+		} */ //posso fare semplicemente così
+		return x % 2 == 0;
+	}
+
+	public static void printLenAndUpperCaseLowerCase(String sentence) { //nome troppo lungo, andrebbe fatto in più funzioni
+			int stringLen = sentence.length();  //lunghezza della stringa
+			System.out.println(stringLen);  //volendo potrei direttamente stampare sentence.length(), senza creare la variabile
+			String lower = sentence.toLowerCase();
+			String upper = sentence.toUpperCase();
+			System.out.println(lower);
+			System.out.println(upper);
+	}
+
+	public static String getLonger(String s1, String s2) {
+/*		if (s1.length() > s2.length()) {
+			return s1;
+		} else {
+			return s2;  //se sono uguali in questo caso torna la seconda
+		}*/
+
+		//posso scriverlo più compatta utilizzando l'operatore ternario
+		return s1.length() > s2.length() ? s1 : s2;  //se la booleana è vera torna la cosa a sinistra dei : , altrimenti torna quella a destra
+		//prima di ? e : posso utilizzare qualsiasi condizione booleana
 	}
 
 }
