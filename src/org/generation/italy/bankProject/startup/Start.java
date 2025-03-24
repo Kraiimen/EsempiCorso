6df59cfcedb962ccb4e2a3a835d777937c4487fc
Zomerzz <aresufilippo2005@gmail.com>
2025-03-24 18:11:35 +0100

    commit automatico

--- src/org/generation/italy/bankProject/startup/Start.java
+++ src/org/generation/italy/bankProject/startup/Start.java
@@ -6,9 +6,9 @@ public class Start {
     public static void main(String[] args) {
         Account x = new Account(); // creiamo un oggetto di tipo Account
         x.printBalance(); // se printassimo il valore di balance, che non è stato inizializzato nella classe uscirebbe 0.00
-        x.deposit(100);
-        x.printBalance();
-        x.withdraw(60);
-        x.printBalance();
+        x.deposit(100); // andiamo ad aggiungere 100 al bilancio usando la funzione deposit
+        x.printBalance(); // adesso che il valore è stato modificato se printiamo uscirà 100
+        x.withdraw(60); // adesso invece ritiriamo 60 con la funzione withdraw 
+        x.printBalance(); // e printassimo il nuovo bilandio sarà 100-60
     }
 }
