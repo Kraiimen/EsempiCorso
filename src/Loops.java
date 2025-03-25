c98e65f9fdd94410e336206a3fac34423789d4bb
pixscar <pie.dx96@gmail.com>
2025-03-25 10:35:37 +0100

    Fixed Loops.java

--- src/Loops.java
+++ src/Loops.java
@@ -1,13 +1,12 @@
-/*import java.io.Console;
+import java.io.Console;
 
 public class Loops {
     public static void main(String[] args) {
         Console console = System.console();
-		}
-        for( int i = 0 ; i < 10 ; i++ ) { //nel secondo slot sto dicendo dove terminare il ciclo
+        for (int i = 0; i < 10; i++) { //nel secondo slot sto dicendo dove terminare il ciclo
             System.out.println(i);
         }
-        System.out.println("ciclo terminato"); 
+        System.out.println("ciclo terminato");
         // fare ciclo for che stampa i numeri dispari da 1 a 100
 
         // for(int i = 1 ; i < 101 ; i++ ) { 
@@ -16,34 +15,35 @@ public class Loops {
         //     } //se il resto della divisione è 0 i numeri sono pari
         // }
 
-        for(int i = 100; i > 0; i -= 5) {  //questo stampa tutti i numeri da 100 a 0 tutti multipli di 5
+        for (int i = 100; i > 0; i -= 5) {  //questo stampa tutti i numeri da 100 a 0 tutti multipli di 5
             System.out.println(i);
         }
 
         int y = 100;
-        while(y > 0) {
+        while (y > 0) {
             System.out.println(y);
             y -= 5;
         }
 
         int z = 100;
-        do { 
+        do {
             System.out.println(z);
             z -= 5;
         } while (z > 0);
-        
+
         String answer;
-        do { 
+        do {
             System.out.print("inserisci una risposta di tipo Yes or No");
             answer = console.readLine();
-            if(!answer.equals("Yes") && !answer.equals("No")) {
+            if (!answer.equals("Yes") && !answer.equals("No")) {
                 System.out.println("Ti ho detto di rispondere Yes or No!");
             }
-        while (!answer.equals("Yes") && !answer.equals("No"));
-		}
+        } while (!answer.equals("Yes") && !answer.equals("No")) ;
 
-       // System.out.println("dammi un numero qualsiasi");
-        //String ns = console.readLine();
-        //int n = Integer.parseInt(ns); //passami in intero la stringa ns
-       // System.out.println(n + 1);
-} */
+
+            // System.out.println("dammi un numero qualsiasi");
+            //String ns = console.readLine();
+            //int n = Integer.parseInt(ns); //passami in intero la stringa ns
+            // System.out.println(n + 1);
+    }
+}
