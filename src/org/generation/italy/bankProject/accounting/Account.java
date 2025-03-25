0d029506944bce8f6b732dfbb6d96ab5adb10655
LorenzoCoretti1 <lorenzo.coretti1@gmail.com>
2025-03-25 14:26:20 +0100

    Continua l'implementazione degli esempi ad oggetti sui conti correnti

--- src/org/generation/italy/bankProject/accounting/Account.java
+++ src/org/generation/italy/bankProject/accounting/Account.java
@@ -2,24 +2,61 @@ package org.generation.italy.bankProject.accounting;
 
 import java.time.LocalDate;
 
-public class Account {
-    double balance;
-    int id;
+public class Account {  //la visibilità di default vuol dire visibile all'interno del suo package
+    //in un file java può essere presente solo una classe pubblica che deve essere chiamata come il file
+    //private = visibile solo nella classe che lo definisce (utilizzato prettamente per le variabili) private != cybersecurity
+    public double bal;
+    private int id;
     // Quando il compilatore vede il nome di una classe si chiede "Questa classe viene da Java.lang?" ovvero il package base del linguaggio java
     // se questo non è vero, si chiede "è dello stesso package di questa classe?" quindi se in un file si vuole usare una classe che sta nello stesso package del file, non c'è bisogno di importarla
     // se non la trova nemmeno li controllerà la lista degli import ad inizio file
-    LocalDate creationDate;
+    private LocalDate creationDate;
+
+    public Account(){
+
+    } //invoco il costruttore (non necessario crearlo in quanto in assenza di costruttori in una classe il compilatore usa quello di default)
+    // non c'è il tipo di ritorno perché il costruttore ritorna SEMPRE l'indirizzo dell'oggetto
+    //il costruttore va sull' heap e mi alloca la memoria per contenere l'oggetto e inizializza le variabili dell'oggetto con i valori di default poi mi restituisce l'indirizzo
+    //Inzializza a valori di default le variabili dell'oggetto
 
     public void printBalance(){
-        System.out.printf("Il conto con id %d ha come saldo %f%n", id, balance);
+        System.out.printf("Il conto con id %d ha come saldo %f%n", id, bal);
     }
 
     public double deposit(double amount){
-        balance += amount;
-        return balance;
+        bal += amount;
+        return bal;
     }
     public double withdraw(double amount){
-        balance -= amount;
-        return balance;
+        doInternalOperation();
+        bal -= amount;
+        return bal;
     }
+    private void doInternalOperation(){
+
+    }
+    public double getBalance(){
+        return bal / 100;
+    }
+    public void setBalance(double newBalance){
+        if(newBalance <= 0){
+            return;
+        }
+        bal = newBalance;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate cr) {
+        creationDate = cr;
+    }
+
+
 }
+
+
+//lo stato di una classe è l'insieme delle sue variabili
+//una variabile è privata se non vogliamo che il resto del programma sappia che esiste perché la vogliamo modificare come vogliamo
+//quindi le variabili degli oggetti (lo stato della classe) dovrebbero di norma sempre essere private
