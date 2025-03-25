418fa5cd9b18aea0d1d482552fdfae1d0e987d01
martapetruzzelli <martapetruzzelli1@gmail.com>
2025-03-25 10:23:00 +0100

    aggiunge esercizi creazione classi

--- src/org/generation/italy/bankProject/startup/Start.java
+++ src/org/generation/italy/bankProject/startup/Start.java
@@ -1,5 +1,16 @@
 package org.generation.italy.bankProject.startup;
 
+import java.time.LocalDate;
+
+import org.generation.italy.bankProject.accounting.Account;
+
 public class Start {
-    
+    public static void main(String[] args) {
+        Account x = new Account(2, 1, LocalDate.now()); // creiamo un oggetto di tipo Account
+        x.printBalance(); // se printassimo il valore di balance, che non è stato inizializzato nella classe uscirebbe 0.00
+        x.deposit(100);
+        x.printBalance();
+        x.withdraw(60);
+        x.printBalance();
+    }
 }
