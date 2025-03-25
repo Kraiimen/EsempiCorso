2187571ff9ee5ddd93520225e567697cbd10c9d6
N9C9 <nicolo.casertano@gmail.com>
2025-03-25 18:00:54 +0100

    continua l'introduzione all'oo con la classe account e movement

--- src/org/generation/italy/bankProject/accounting/Account.java
+++ src/org/generation/italy/bankProject/accounting/Account.java
@@ -1,28 +1,52 @@
 package org.generation.italy.bankProject.accounting;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
-public class Account {  //la visibilità di default vuol dire visibile all'interno del suo package
+public class Account {
+    //la visibilità di default vuol dire visibile all'interno del suo package
     //in un file java può essere presente solo una classe pubblica che deve essere chiamata come il file
     //private = visibile solo nella classe che lo definisce (utilizzato prettamente per le variabili) private != cybersecurity
+
+
+    //ATTRIBUTI or FIELDS or VARIABILI DEGLI OGGETTI
+    private static int lastId; //usando "static" ogni conto avrà il suo lastId
     public double bal;
     private int id;
+    private LocalDate creationDate;
+    private ArrayList movements;
     // Quando il compilatore vede il nome di una classe si chiede "Questa classe viene da Java.lang?" ovvero il package base del linguaggio java
     // se questo non è vero, si chiede "è dello stesso package di questa classe?" quindi se in un file si vuole usare una classe che sta nello stesso package del file, non c'è bisogno di importarla
     // se non la trova nemmeno li controllerà la lista degli import ad inizio file
-    private LocalDate creationDate;
 
-    public Account(){
 
-    } //invoco il costruttore (non necessario crearlo in quanto in assenza di costruttori in una classe il compilatore usa quello di default)
+    //COSTRUTTORI
+    public Account() {
+        lastId++;
+        id = lastId;
+//        this(0);
+        creationDate = LocalDate.now();
+    }
+    public Account(double initialBalance) {
+//        lastId++;
+//        id = lastId;
+        this();
+        bal = initialBalance;  //Account è il nome di tutti e due i costruttori, ma li differenzierò perchè avranno parametri diversi
+    }
+    public Account(double initialBalance, LocalDate creationDate) {
+        this(initialBalance);
+        this.creationDate = creationDate;
+    }
+    //invoco il costruttore (non necessario crearlo in quanto in assenza di costruttori in una classe il compilatore usa quello di default)
     // non c'è il tipo di ritorno perché il costruttore ritorna SEMPRE l'indirizzo dell'oggetto
     //il costruttore va sull' heap e mi alloca la memoria per contenere l'oggetto e inizializza le variabili dell'oggetto con i valori di default poi mi restituisce l'indirizzo
     //Inzializza a valori di default le variabili dell'oggetto
 
+
+    //FUNZIONI - prima quelle importanti poi le getter o setter
     public void printBalance(){
         System.out.printf("Il conto con id %d ha come saldo %f%n", id, bal);
     }
-
     public double deposit(double amount){
         bal += amount;
         return bal;
@@ -44,11 +68,9 @@ public class Account {  //la visibilità di default vuol dire visibile all'inter
         }
         bal = newBalance;
     }
-
     public LocalDate getCreationDate() {
         return creationDate;
     }
-
     public void setCreationDate(LocalDate cr) {
         creationDate = cr;
     }
