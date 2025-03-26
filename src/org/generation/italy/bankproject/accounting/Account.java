a424a617fa1360762798391ac3da727a53c6c1f6
pixscar <pie.dx96@gmail.com>
2025-03-26 14:10:34 +0100

    Inserisce i primi metodi che lavorano con i movimenti dell'account

--- src/org/generation/italy/bankproject/accounting/Account.java
+++ src/org/generation/italy/bankproject/accounting/Account.java
@@ -1,6 +1,7 @@
 package org.generation.italy.bankproject.accounting;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Account {
@@ -20,6 +21,7 @@ public class Account {
 		lastId++; // Il primo id del primo oggetto sarà 1, poi 2, 3, ecc.
 		id = lastId;
 		creationDate = LocalDate.now(); // Data della creazione del conto. Now è un metodo di una classe, un metodo statico della classe Localdate.
+		movements = new ArrayList(); // Ogni volta che utilizzo il costruttore per creare un account verrà creata una lista di movimenti dell'account
 	}
 
 	public Account(double balance) {
@@ -37,17 +39,46 @@ public class Account {
 
 	// METODI
 	public void printBalance() {
-		System.out.printf("Il conto con id %d ha come saldo %f%n", id, balance);
+//		System.out.println("Durante questa esecuzione di printBalance this è uguale a " + this);   Per dimostrare che il valore di this alla chiamata coincide con l'indirizzo dell'oggetto con cui ho chiamato la funzione
+		System.out.printf("Il conto con id %d ha come saldo %f%n", this.id, this.balance); // il this non è obbligatorio, ma comunque è implicito, perché è un puntatore all'oggetto su cui chiamo il metodo
 	}
 
 	public double deposit(double amount) {
+		Movement move = new Movement(amount, balance, LocalDateTime.now(), MovementType.DEPOSIT); // Creo un nuovo oggetto di tipo Movement
+		movements.add(move); // Registro la 'mossa' move nell'ArrayList, per ora non usiamo le generics.
 		balance += amount;
 		return balance;
 	}
 
 	public double withdraw(double amount) {
+		Movement move = new Movement(amount, balance, LocalDateTime.now(), MovementType.WITHDRAWAL);
+		movements.add(move);
 		balance -= amount;
 		return balance;
+	}
+
+	// Voglio fare un metodo che mi dica il totale delle somme dei depositi
+	// I depositi posso prenderli dall'arraylist, per cui nessun input è necessario
+	public double getSumDeposits() {
+		double sum = 0;
+//		movements.add(0, "Pippo");  //Per vedere che mettendo una Stringa dentro l'arraylist senza generics e castando forzatamente m1 mando il programma in crash
+		// Non mi serve sapere la posizione in cui mi trovo nell'arraylist, per cui posso usare un for each, ma non usando le generics ho un problema
+		// Per prendere gli elementi dall'arraylist esiste un metodo get, che però
+//		Movement m1 = movements.get(0); // Così prenderei ciò che sta nell'arraylist, ma non usando i generics lui pensa che conterrà Object, una classe di Java da cui tutte le classi ereditano, tutte le classi sono discendenti da Objet, una sorta di oggetto di base
+		// Non mi fa assegnare ciò che è dentro all'arraylist a m1 perché il compiler non è certo che dentro ci sia un Movement
+		// Fare ciò sarebbe corretto ma non avendo utilizzato i generics il compiler si oppone
+		// Il compiler vorrebbe questo di giù
+//		Object m1 = movements.get(0); // facendo così se m1 è visto come Object però non posso utilizzare i metodi di Movement
+		// Farò quindi un cast forzato come giù
+//		Movement m1 = (Movement)movements.get(0); // Cast forzato a Movement della stringa "Pipopo" memorizzata alla posizione 0 dell'arraylist in modo da poter utilizzare ciò che sta nell'arraylist
+		for(int i = 0; i < movements.size(); i++){
+			Object ob = movements.get(i);
+			Movement m = (Movement)ob;
+			if (m.getType() == MovementType.DEPOSIT) {
+				sum += m.getAmount(); //Se l'if è rispettato vado a sommare al totale l'amount dell'oggetto Momevent
+			}
+		}
+		return sum;
 	}
 
 	private void doInternalOperation() {
@@ -70,8 +101,8 @@ public class Account {
 		return creationDate;
 	}
 
-	public void setCreationDate(LocalDate cr) {
-		creationDate = cr;
+	public void setCreationDate(LocalDate creationDate) {
+		this.creationDate = creationDate;                  // In un metodo il this significa il campo creationDate del metodo che sto costruendo
 	}
 
 
--- src/org/generation/italy/bankproject/startup/Start.java
+++ src/org/generation/italy/bankproject/startup/Start.java
@@ -10,7 +10,8 @@ public class Start {
 	public static void main(String[] args) {
 		Account x = new Account(); // Sto creando un oggetto di tipo Account di nome x
 		x.printBalance();
-		x.deposit(100);
+		x.deposit(200);
+		x.deposit(100);  // Secondo deposito
 		x.printBalance();
 		x.withdraw(60);
 		x.printBalance();
@@ -21,9 +22,25 @@ public class Start {
 		// Inoltre se cambio il nome di balance in bal nella classe ad esempio dovrei andare a cambiarne il nome
 		// ovunque, mentre se fosse privata non avrei questo problema
 
+		x.setBalance(100);
+
 		Movement m1 = new Movement(100, 10000, LocalDateTime.now(), MovementType.DEPOSIT); // Sto creando un movimento tramite un costruttore Movement
 		Movement m2 = new Movement(100, 10000, LocalDateTime.now(), MovementType.WITHDRAWAL);
+		Movement m3 = new Movement(200, 10000, LocalDateTime.now(), MovementType.DEPOSIT);      //Questi non li sto utilizzando
+		Account z = new Account(10000);
+//		System.out.println("L'indirizzo di z è " + z);    Per stampare l'indirizzo dell'oggetto e dimostrare che this abbia lo stesso indirizzo quando chiamo la funzione sull'oggetto
+		Account w = new Account(20000);
+//		System.out.println("L'indirizzo di w è " + w);
+//		System.out.println(z.balance);  Posso chiamarle solo con balance public
+//		System.out.println(w.balance);
+//		System.out.println("Sto per chiamare printBalance su z facendo z.printBalance");
+		z.printBalance();
+//		System.out.println("Sto per chiamare printBalance su w facendo w.printBalance");
+		w.printBalance();
 		// Se vuoi copiare una riga giù metti il segno sulla riga e poi cmd + d
+
+		double depositSum = x.getSumDeposits();
+		System.out.println("Il totale depositato è: " + depositSum);
 
 	}
 }
