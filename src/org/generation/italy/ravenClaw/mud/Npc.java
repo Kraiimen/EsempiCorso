76e8f72e6860ebac34bb9e1a8a933ad3f6b86832
LorenzoCoretti1 <lorenzo.coretti1@gmail.com>
2025-03-27 17:46:25 +0100

    toglie tutte le cose non ravenclaw e aggiunge i file del videogioco

---
+++ src/org/generation/italy/ravenClaw/mud/Npc.java
@@ -0,0 +1,25 @@
+package org.generation.italy.ravenClaw.mud;
+
+public class Npc extends Entity{
+    private int xpOnDeath;
+    private boolean canBeAttacked;
+
+    public Npc(String name, int maxHp, int attackDamage, int xpOnDeath, boolean canBeAttacked){
+        super(name, maxHp, attackDamage);
+        this.xpOnDeath = xpOnDeath;
+        this.canBeAttacked = canBeAttacked;
+    }
+    public void giveInstructions(){
+        if(canBeAttacked){
+            System.out.println("Uccidimi ti prego attaccami fammi male");
+        }else {
+            System.out.println("noooo non uccidermi sono tuo amico ti voglio bene");
+        }
+    }
+
+
+
+
+
+
+}
