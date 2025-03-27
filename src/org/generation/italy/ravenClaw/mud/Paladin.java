99201f3e7c10e173947f6cb2175dbbf65b8d9fdb
N9C9 <nicolo.casertano@gmail.com>
2025-03-27 21:13:42 +0100

    spiega e riassume il lavoro fatto sulla libreria

---
+++ src/org/generation/italy/ravenClaw/mud/Paladin.java
@@ -0,0 +1,10 @@
+package org.generation.italy.ravenClaw.mud;
+
+public class Paladin extends Player {
+
+    private boolean isVergin = true;
+
+    public Paladin(String name, int maxHp, int attackDamage, String playerName) {
+        super(name, maxHp, attackDamage, playerName)
+    }
+}
