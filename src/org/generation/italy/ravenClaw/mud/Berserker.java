138dcaa41d632559f6496ca8e06b1e574a31acac
CamillaMar <camillamarchioro@fadbrera.edu.it>
2025-03-27 18:06:13 +0000

    aggiunge la classe Berserker

---
+++ src/org/generation/italy/ravenClaw/mud/Berserker.java
@@ -0,0 +1,22 @@
+package org.generation.italy.ravenClaw.mud;
+
+import java.util.Random;
+
+public class Berserker extends Player{
+    public Berserker(String name, int maxHp, int attackDamage, String playerName){
+        super(name, maxHp, attackDamage, playerName);
+    }
+
+    @Override
+    public void attack(Entity attacked){
+        int damageMade = getAttackDamage();
+        Random dice = new Random();
+        int x = dice.nextInt(9);
+        if(x == 0){
+            damageMade *= 2;
+            System.out.println("Berserk mode activated.");
+        }
+        attacked.hurt(damageMade);
+        System.out.printf("%s is attacking %s for %d damage", getName(), attacked.getName(), damageMade);
+    }
+}
