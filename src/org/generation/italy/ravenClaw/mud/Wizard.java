4cfc626075d0e4e8ba48a4d4a8f79ea61f424686
Zomerzz <aresufilippo2005@gmail.com>
2025-03-27 18:50:18 +0100

    aggiunge la classe mago

--- src/org/generation/italy/ravenClaw/mud/Wizard.java
+++ src/org/generation/italy/ravenClaw/mud/Wizard.java
@@ -3,23 +3,33 @@ package org.generation.italy.ravenClaw.mud;
 public class Wizard extends Player {
 
     private int intelligence;
+    private int spellPoints;
 
     public Wizard (String name, int maxHp, int attackDamage,String playerName,int intelligence ){
         super(name, maxHp, attackDamage,playerName);
         if(intelligence >=0 && intelligence<=100){
             this.intelligence = intelligence;
         }
+        spellPoints = (int)intelligence/2;
     }
 
-    public void castFireball(Entity attacked){
-        int damageModifier = 4;
-        System.out.printf("%s is Casting a fireball on %s for %d damage", this.getName(), attacked.getName(),attack(attacked,damageModifier));
-    }
+    public int castFireball(Entity attacked){
+        int damageMade= this.getAttackDamage();
+        if(spellPoints>0){
+            spellPoints-=5;
+            damageMade *= 4;
+            System.out.printf("%s is Casting a fireball on %s for %d damage", this.getName(), attacked.getName(),damageMade);
+        }else{
+            damageMade *= 0;
+            System.out.printf("%s tried to cast a fireball on %s but failed", this.getName(), attacked.getName());
+        }
 
-    public int attack(Entity attacked, int damageModifier){
-        int damageMade = getAttackDamage() * damageModifier;  //variabile modificabile che prende come minimo il danno dell'attaccante
-        attacked.hurt(damageMade);
         return damageMade;
+    }
+    @Override
+    public void attack(Entity attacked){
+        int damageMade = castFireball(attacked);
+        attacked.hurt(damageMade);
 
     }
 
