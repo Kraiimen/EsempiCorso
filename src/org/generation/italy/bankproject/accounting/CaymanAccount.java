2bbc2a079ddc2df22648d5f8d2e93d5ee431357a
elvis_la_fata <elvislafata@gmail.com>
2025-03-28 09:02:46 +0100

    Update 26/03/25 fatto qualche esercizio extra su ArrayExercises. continuata la spiegazione sugli oggetti

---
+++ src/org/generation/italy/bankproject/accounting/CaymanAccount.java
@@ -0,0 +1,35 @@
+package org.generation.italy.bankproject.accounting;
+
+import java.time.LocalDate;
+
+public class CaymanAccount extends Account{
+    private String secretCode;
+
+    public CaymanAccount(){
+//        super(10_000);
+//        secretCode = "xxx";
+        this(10_000);
+    }
+    public CaymanAccount(double balance){
+        super(balance);
+        secretCode = "xxx";
+    }
+    public CaymanAccount(double balance, LocalDate creationDate,String secretCode){
+        super(balance,creationDate);
+        this.secretCode = secretCode;
+    }
+
+
+
+
+    public void evadeTax(){
+        System.out.println("Evado le tasse con l'aiuto del commercialista Elvis");
+    }
+
+    @Override
+    public double deposit(double amt){
+        evadeTax();
+        balance += amt;
+        return balance;
+    }
+}
