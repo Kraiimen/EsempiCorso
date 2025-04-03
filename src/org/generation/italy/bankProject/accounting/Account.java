514453315d95d11699ea45305a603f667092c88c
N9C9 <nicolo.casertano@gmail.com>
2025-04-03 17:19:24 +0200

    aggiorna prim del pull dal push di Riccardo, exceptionals

--- src/org/generation/italy/bankProject/accounting/Account.java
+++ src/org/generation/italy/bankProject/accounting/Account.java
@@ -1,5 +1,7 @@
 package org.generation.italy.bankProject.accounting;
 
+import org.generation.italy.bankProject.accounting.accountType.Movement;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
