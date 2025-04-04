9bc19bb7c269d82c7dc97191daf59ab6e3ccccf1
N9C9 <nicolo.casertano@gmail.com>
2025-04-04 17:46:14 +0200

    merga dalla main dall'esercizio a RavenClaw

--- src/org/generation/italy/bankProject/accounting/Client.java
+++ src/org/generation/italy/bankProject/accounting/Client.java
@@ -1,9 +1,44 @@
 package org.generation.italy.bankProject.accounting;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Client {
     private String clientCode;
     private String lastName;
     private String firstName;
-    private String birthDate;
+    private LocalDate birthDate;
     //Scegli come il client ha i propri conti
+    ArrayList<Account> ownedAccounts = new ArrayList<>();
+
+    public Client(String clientCode, String lastName, String firstName, LocalDate birthDate) {
+        this.clientCode = clientCode;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.birthDate = birthDate;
+    }
+    public Client(String clientCode, String lastName, String firstName, LocalDate birthDate, Account oAccount) {
+        this.clientCode = clientCode;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.birthDate = birthDate;
+        ownedAccounts.add(oAccount);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null){
+            return false;
+        }
+        if(!(obj instanceof Client)){
+            return false;
+        }
+        Client other = (Client) obj;
+        return other.clientCode.equals(this.clientCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return clientCode.hashCode();
+    }
 }
--- src/org/generation/italy/bankProject/accounting/InMemoryClientRepository.java
+++ src/org/generation/italy/bankProject/accounting/InMemoryClientRepository.java
@@ -1,4 +1,32 @@
 package org.generation.italy.bankProject.accounting;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InMemoryClientRepository implements ClientRepository {
+
+    private HashSet<Client> clientHashSet = new HashSet<>();
+
+
+
+//    @Override
+//    public Client getByClientCode(String clientCode) {
+//        return clientHashSet.contains();
+//    }
+
+    @Override
+    public Set<Client> getByNumAccounts(int numAccounts) {
+        return Set.of();
+    }
+
+    @Override
+    public Set<Client> getByTotalBalance(double balance) {
+        return Set.of();
+    }
+
+    @Override
+    public Set<Client> getAllTaxEvaders() {
+        return Set.of();
+    }
+
 }
