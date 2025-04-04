5d7a675bcd2a862bd3cf0340da0f03c91a259643
Kraiimen <78323935+Kraiimen@users.noreply.github.com>
2025-04-04 21:02:03 +0200

    Cercando di risolvere

--- src/org/generation/italy/bankProject/startup/Start.java
+++ src/org/generation/italy/bankProject/startup/Start.java
@@ -10,7 +10,8 @@ import org.generation.italy.bankProject.accounting.accountType.PlatinumAccount;
 
 import java.time.LocalDate;
 
-
+//Introdurre una classe cliente, un cliente ha un nome, un cognome, una data di nascita, un codice banca(string)
+//un cliente deve avere dei conti
 public class Start {
 
     public static void main(String[] args) {
