0254060a5de604c7481b38d4ec40c195a89a457e
CamillaMar <camillamarchioro@fadbrera.edu.it>
2025-04-04 13:02:06 +0000

    modifica il tipo di ritorno della funzione getAll nella classe AccountRepository

--- src/org/generation/italy/bankProject/accounting/AccountRepository.java
+++ src/org/generation/italy/bankProject/accounting/AccountRepository.java
@@ -3,10 +3,11 @@ package org.generation.italy.bankProject.accounting;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public interface AccountRepository {
 
      int saveAccount(Account a) throws DataException;
-     ArrayList<Account> getAll() throws DataException;//<> Questo ArrayList non lavora più con object, ma con oggetti di classe Account
+     Iterable<Account> getAll() throws DataException;//<> Questo ArrayList non lavora più con object, ma con oggetti di classe Account
      void upDateAccount(Account a);
 }
