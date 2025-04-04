9934d72af22dab2f569df2d70a6ed241c23871bb
elvis_la_fata <elvislafata@gmail.com>
2025-04-04 16:35:15 +0200

    Esempi di set e inizio nuovo esercizio su package accounting

--- src/org/generation/italy/bankProject/accounting/AccountRepository.java
+++ src/org/generation/italy/bankProject/accounting/AccountRepository.java
@@ -2,12 +2,21 @@ package org.generation.italy.bankProject.accounting;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 public interface AccountRepository {
 
      int saveAccount(Account a) throws DataException;
-     Iterable<Account> getAll() throws DataException;//<> Questo ArrayList non lavora più con object, ma con oggetti di classe Account
+     ArrayList<Account> getAll() throws DataException;//<> Questo ArrayList non lavora più con object, ma con oggetti di classe Account
      void upDateAccount(Account a);
+     boolean deleteAccount(int id);
+     Set<Account> getAllMoreActiveThan(int numMovements);
+     Set<Account> getByClientId(int clientId);
+     Set<Account> getByBalanceAndDateRange(double balance, LocalDate start, LocalDate end);
+     Account getAccountById(int id);
+
 }
---
+++ src/org/generation/italy/collections/HashExamples.java
@@ -0,0 +1,19 @@
+package org.generation.italy.collections;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashExamples {
+    public static void main(String[] args){
+        Employee e1 = new Employee("Giorgio", "Vanni", "1");
+        Employee e2 = new Employee("Giorgio", "Vanni", "2");
+        Employee e3 = new Employee("Giorgio", "Vanni", "1");
+
+        Set<Employee> emps = new HashSet<>();
+        emps.add(e1);
+        emps.add(e2);
+        emps.add(e3);
+
+        System.out.println(emps.size());
+    }
+}
