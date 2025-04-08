9e9f66b74fd743582a2df713e9777c0f87f99c95
N9C9 <nicolo.casertano@gmail.com>
2025-04-08 19:57:46 +0200

    Aggiorna 8 aprile

---
+++ src/eserciziRipassoArgomenti/StreamAndLambda.java
@@ -0,0 +1,22 @@
+package eserciziRipassoArgomenti;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamAndLambda {
+
+    static List<Integer> numeri = new ArrayList<>();
+
+    static {
+        numeri.add(1);
+        numeri.add(2);
+        numeri.add(3);
+        numeri.add(4);
+        numeri.add(5);
+        numeri.add(6);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
