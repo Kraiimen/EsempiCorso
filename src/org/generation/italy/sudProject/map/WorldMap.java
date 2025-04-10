b5ca75a072c345b775ef2c449b871055f3fefbc0
EmanueleGiustiniani <giustinianiemanuele0@gmail.com>
2025-04-10 11:44:36 +0200

    Continua il s.u.d.

--- src/org/generation/italy/sudProject/map/WorldMap.java
+++ src/org/generation/italy/sudProject/map/WorldMap.java
@@ -1,19 +1,27 @@
 package org.generation.italy.sudProject.map;
 
+import java.util.LinkedList;
+
 public class WorldMap {
     // /--ATTRIBUTES--/
-
-
-
+    private LinkedList<Room> world;
 
     // /--CONSTRUCTORS--/
-
+    public WorldMap(){
+        world = new LinkedList<>();
+        world.offer(new Room("Temple_Square",false, true,true, false));
+        world.offer(new Room("Market_Square",true, true,true, true));
+        world.offer(new Room("Temple",false, false,false, true));
+        world.offer(new Room("Bakery",false, false,false, true));
+        world.offer(new Room("Armory",false, false,true, false));
+        world.offer(new Room("Garden_1",true, true,false, false));
+        world.offer(new Room("Garden_2",true, true,false, false));
+        world.offer(new Room("Garden_3",true, true,false, false));
+        world.offer(new Room("Woods",true, false,false, false));
+    }
 
 
 
 
     // /--METHODS--/
-
-
-
 }
