76df3ed9843bab4f25f3836ba35d18a0ae73edac
MarcelloCoppolino <marcello95c@gmail.com>
2025-04-11 09:31:41 +0200

    aggiornamento mud game

--- src/capstoneproject/entity/Npc.java
+++ src/capstoneproject/entity/Npc.java
@@ -1,17 +1,26 @@
-//package capstoneproject.entity;
-//
-//import java.util.Map;
-//
-//public abstract class Npc extends Entity{
-//
-//    protected String npcName;
-//    protected boolean isRage;
-//    Map<String,String> npcWord;
-//
-//    public Npc(int maxHealth, int maxDamage, String npcName) {
-//        super(maxHealth, maxDamage);
-//    }
-//
-//    abstract void attack();
-//
-//}
+package capstoneproject.entity;
+
+import capstoneproject.rooms.Room;
+
+import java.util.Map;
+
+public abstract class Npc extends Entity{
+
+    protected String npcName;
+    protected boolean isRage;
+    Map<String,String> npcWord;
+
+    public Npc(String name, int maxHealth, int maxDamage, Room currentRoom) {
+        super(name, maxHealth, maxDamage, currentRoom);
+        this.currentRoom = currentRoom;
+        inizialateStats();
+    }
+    protected abstract void inizialateStats();
+
+    abstract void attack(Entity target);
+
+    public void heal(int amount){
+        healthPoints += amount;
+        if(healthPoints > maxHealth) healthPoints = maxHealth;
+    }
+}
