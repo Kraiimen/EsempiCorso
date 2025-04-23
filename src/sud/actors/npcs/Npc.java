ca672e2992dc879d15bf3c0e0128ba0ae8500e8d
Federico-95 <federico.desimone@gmail.com>
2025-04-23 22:36:52 +0200

    aggiunge fix temporanei gioco sud

--- src/sud/actors/npcs/Npc.java
+++ src/sud/actors/npcs/Npc.java
@@ -4,29 +4,38 @@ import sud.Die;
 import sud.actors.Actor;
 import sud.environment.rooms.Room;
 
-public class Npc extends Actor {
-    private boolean isInteractable;
+public class Npc /*extends Actor*/ {
+    //private boolean isInteractable;
 
+    private Actor body;
 
-    public Npc(String name) {
-        super(name);
-    }
-    public Npc(String name, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma) {
-        super(name, strength, dexterity, constitution, intelligence, wisdom, charisma);
-    }
-    public Npc(String name, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma, Room spawnPoint) {
-        super(name, strength, dexterity, constitution, intelligence, wisdom, charisma, spawnPoint);
+    public Npc(Actor a) {
+        body = a;
     }
 
-    public boolean CanInteract() {
-        return isInteractable;
-    }
+    //costruttore ipotetico con reflection... poi mi sono reso conto che non c'è nessun motivo di usare reflection...
+    //public Npc(String name, Class<? extends Actor> a) {
+    //    try {
+    //        body = a.getDeclaredConstructor(String.class).newInstance(name);
+    //    } catch (Exception e) {
+    //        e.printStackTrace(); // or handle it as needed
+    //    }
+    //}
+    //public Npc(String name, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma) {
+    //    super(name, strength, dexterity, constitution, intelligence, wisdom, charisma);
+    //}
+    //public Npc(String name, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma, Room spawnPoint) {
+    //    super(name, strength, dexterity, constitution, intelligence, wisdom, charisma, spawnPoint);
+    //}
 
-    public void setIsInteractable(boolean canInteract) {
-        this.isInteractable = canInteract;
-    }
+    //public boolean CanInteract() {
+    //    return isInteractable;
+    //}
 
-    @Override
+    //public void setIsInteractable(boolean canInteract) {
+    //    this.isInteractable = canInteract;
+    //}
+
     public Die getConstitutionDice() {
         return null;
     }
