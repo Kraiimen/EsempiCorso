9e7b6077fee259e0895e2fbee3f8d8ceaab93bdf
OniTusk <lazyorpheus@gmail.com>
2025-04-24 12:58:33 +0200

    aggiunge esempi di jdbc

---
+++ src/org/generation/italy/jbdc/Examples.java
@@ -0,0 +1,38 @@
+package org.generation.italy.jbdc;
+
+import org.postgresql.jdbc.PgConnection;
+
+import java.sql.*;
+
+import org.postgresql.Driver;
+
+public class Examples {
+    public static void main(String[] args){
+//        try{
+//            Class.forName("org.postgresql.Driver");
+//        } catch (ClassNotFoundException e){
+//            System.out.println("Driver jdbc non configurato correttamente");
+//            System.exit(-1);
+//        }
+//        Driver d = null;
+
+//        String url = "jdbc:postgresql://localhost:5432/company";
+//        String user = "postgresMaster";
+//        String password = "goPostgresGo";
+//
+        String query = "SELECT productid AS id, productname, unitprice FROM products";
+
+        try(Connection con = JbdcConnectionFactory.createConnection(); // Simple Factory Idiom. funzione statica.
+            Statement st = con.createStatement()){ //Factory method pattern
+            System.out.println("Connessione stabilita");
+            System.out.println(con.getClass().getName());
+            System.out.println(st.getClass().getName());
+            ResultSet rs = st.executeQuery(query); // Factory method pattern
+            while(rs.next()){
+                System.out.printf("id: %d productname: %s unitprice: %f%n" , rs.getInt("id"), rs.getString("productname"), rs.getDouble("unitprice"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/org/generation/italy/jbdc/JbdcConnectionFactory.java
@@ -0,0 +1,15 @@
+package org.generation.italy.jbdc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JbdcConnectionFactory {
+    private static final String URL ="jdbc:postgresql://localhost:5432/company";
+    private static final String USER = "postgresMaster";
+    private static final String PASSWORD = "goPostgresGo";
+
+    public static Connection createConnection() throws SQLException {
+        return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
+}
