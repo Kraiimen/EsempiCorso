24a9ca901f643d08b6c89cd2e0a46026c4057949
Kraiimen <78323935+Kraiimen@users.noreply.github.com>
2025-04-24 12:58:58 +0200

    Aggiunge esempio JDBC e SQL

---
+++ src/jdbc/Examples.java
@@ -0,0 +1,34 @@
+package jdbc;
+
+import org.postgresql.Driver;
+
+import java.sql.*;
+// import org.postgresql.Driver;
+
+public class Examples {
+    public static void main(String[] args) {
+//        try {
+//            Class.forName("org.postgresql.Driver");
+//        } catch (ClassNotFoundException e) {
+//            System.out.println("Driver JDBC non configurato correttamente!");
+//            System.exit(-1);
+//        }
+
+//        Driver d = null;
+        String query = "SELECT productid, productname, unitprice FROM products";
+
+        try (Connection con = JdbcConnectionFactory.createConnection(); // Simple Factory Idiom
+             Statement st = con.createStatement()) { // Factory Method Pattern
+            System.out.println("Connessione stabilita!");
+            System.out.println(con.getClass().getName());
+
+            System.out.println(st.getClass().getName());
+            ResultSet rs = st.executeQuery(query); // Factory Method Pattern
+            while(rs.next()){
+                System.out.printf("productid: %d productname: %s unitprice: %f%n", rs.getInt("productid"), rs.getString("productname"), rs.getDouble("unitprice"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
