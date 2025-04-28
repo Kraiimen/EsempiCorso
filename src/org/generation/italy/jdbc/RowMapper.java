1dd2917d176fe140fac3abacf05bc76dd23d7151
Vittorio787 <vittorioaquila.nervi@gmail.com>
2025-04-28 11:35:17 +0200

    Continua esempio su jdbc

--- src/org/generation/italy/jdbc/RowMapper.java
+++ src/org/generation/italy/jdbc/RowMapper.java
@@ -1,5 +1,9 @@
 package org.generation.italy.jdbc;
 
-public interface RowMapper {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+@FunctionalInterface
+public interface RowMapper<T> {
+    T mapRow(ResultSet rs) throws SQLException;
 }
