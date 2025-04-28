179367a17120c9e35672b068e6a0808a0eefe924
Vittorio787 <vittorioaquila.nervi@gmail.com>
2025-04-28 09:07:09 +0200

    Corregge i primi esempi su jdbc

--- src/org/generation/italy/jdbc/Product.java
+++ src/org/generation/italy/jdbc/Product.java
@@ -6,7 +6,39 @@ public class Product {
     private String productName;
     private int supplierId;
     private int categoryId;
-    private int discontinued;
+    private double unitPrice;
+    private int discountinued;
 
-    public 
+    public Product(int productId, String productName, int supplierId, int categoryId, double unitPrice, int discountinued) {
+        this.productId = productId;
+        this.productName = productName;
+        this.supplierId = supplierId;
+        this.categoryId = categoryId;
+        this.discountinued = discountinued;
+        this.unitPrice = unitPrice;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getSupplierId() {
+        return supplierId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public int getDiscountinued() {
+        return discountinued;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
 }
