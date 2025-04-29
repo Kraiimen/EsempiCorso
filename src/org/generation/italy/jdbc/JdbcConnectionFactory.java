c256d7c72d5e1eca69218867f8e937c01375dc46
EmanueleGiustiniani <giustinianiemanuele0@gmail.com>
2025-04-29 10:49:24 +0200

    aggiunge file

---
+++ src/org/generation/italy/jdbc/JdbcConnectionFactory.java
@@ -0,0 +1,17 @@
+package org.generation.italy.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnectionFactory {
+    private static final String URL = "jdbc:postgresql://localhost:5432/company";
+    private static final String USER = "postgresMaster";
+    private static final String PASSWORD = "goPostgresGo";
+
+    public static Connection createConnection() throws SQLException {
+        Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+        con.setAutoCommit(false);
+        return con;
+    }
+}
