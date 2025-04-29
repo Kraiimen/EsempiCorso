c11e5a0e0db55957486ccb01c92ad4345340f715
EmanueleGiustiniani <giustinianiemanuele0@gmail.com>
2025-04-29 10:39:18 +0200

    elimina dei file

--- src/org/generation/italy/jdbc/DataException.java
+++
@@ -1,8 +0,0 @@
-package org.generation.italy.jdbc;
-
-public class DataException extends Exception {
-
-    public DataException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
