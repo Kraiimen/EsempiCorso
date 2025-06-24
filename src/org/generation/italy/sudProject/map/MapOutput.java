e4178a42f1ba50ab44ee31262876a817223f6d8a
EmanueleGiustiniani <giustinianiemanuele0@gmail.com>
2025-06-24 15:54:07 +0200

    aggiunge il player nella mappa

--- src/org/generation/italy/sudProject/map/MapOutput.java
+++ src/org/generation/italy/sudProject/map/MapOutput.java
@@ -24,9 +24,17 @@ public class MapOutput {
             sb.append(getStandardLine());
         }
 
-        // 4 righe centrali (righe 11–14) con eventuali porte est/ovest
+        // 4 righe centrali: inseriamo lo stickman su righe 2, 3 e 4 (i = 1, 2, 3)
         for (int i = 0; i < 4; i++) {
-            sb.append(getDoorLine(e, o));
+            if (i == 1) {
+                sb.append(getDoorLineWithStickman(e, o, 1)); // testa
+            } else if (i == 2) {
+                sb.append(getDoorLineWithStickman(e, o, 2)); // busto e braccia
+            } else if (i == 3) {
+                sb.append(getDoorLineWithStickman(e, o, 3)); // gambe
+            } else {
+                sb.append(getDoorLine(e, o));
+            }
         }
 
         // Ultime 9 righe normali
@@ -63,6 +71,47 @@ public class MapOutput {
 
         if (east) {
             line.append(" "); // Porta aperta a est
+        } else {
+            line.append("|");
+        }
+
+        line.append("\n");
+        return line.toString();
+    }
+
+    // Riga con stickman centrato nella stanza
+    private static String getDoorLineWithStickman(boolean east, boolean west, int stickmanLine) {
+        StringBuilder line = new StringBuilder();
+
+        if (west) {
+            line.append(" "); // apertura ovest
+        } else {
+            line.append("|"); // muro ovest
+        }
+
+        // 22 spazi prima dello stickman
+        line.append("                      ");
+
+        // Corpo dello stickman
+        switch (stickmanLine) {
+            case 1:
+                line.append("  O  "); // testa
+                break;
+            case 2:
+                line.append(" /|\\ "); // busto e braccia
+                break;
+            case 3:
+                line.append(" / \\ "); // gambe
+                break;
+            default:
+                line.append("     ");
+        }
+
+        // 23 spazi dopo lo stickman
+        line.append("                       ");
+
+        if (east) {
+            line.append(" "); // apertura est
         } else {
             line.append("|");
         }
